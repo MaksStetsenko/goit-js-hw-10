a929ab8737ca0e49985b241e58ce949f4ee84a97
MaksStetsenko <106399273+MaksStetsenko@users.noreply.github.com>
2022-11-10 19:16:42 +0200

    ver 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountry } from './fetchCountry';
+
+//==============================================
+
+const refs = {
+  searchBox: document.getElementById('search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
+
+//===============================================
+
+const showCountry = country => {
+  if (country.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    clearMarkUp();
+  }
+  if (country.length > 1 && country.length <= 10) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = renderList(country);
+  }
+  if (country.length === 1) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = renderInfo(country[0]);
+  }
+};
+
+const clearMarkUp = () => {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+};
+
+const renderList = country => {
+  const { name, flags } = country;
+  return country
+    .map(({ name, flags }) => {
+      return `
+      <li class='country-item'>
+        <img src ='${flags.svg}' alt = '${name.official}' class='flag' width = 50px>${name.official}
+      </li>`;
+    })
+    .join('');
+};
+
+const renderInfo = country => {
+  const { name, flags, capital, population, languages } = country;
+  return `
+    <div class='country-inform'>
+        <img
+            src ='${flags.svg}' 
+            alt = '${name.official}' 
+            width = 60px>
+        <h1 class='title'>${name.official}</h1>
+    </div>
+    <div>
+        <p class='description'>Capital : ${capital}</p>
+        <p class='description'>Population : ${population}</p>
+        <p class='description'>Languages : ${Object.values(languages)}</p>
+    </div>`;
+};
+
+const error = () => {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+};
+
+//===============================================
+
+refs.searchBox.addEventListener(
+  'input',
+  debounce(() => {
+    const countryForSearch = refs.searchBox.value.trim().toLowerCase();
+    if (countryForSearch.length > 0) {
+      fetchCountry(countryForSearch).then(showCountry).catch(error);
+    } else {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+  }, DEBOUNCE_DELAY)
+);
